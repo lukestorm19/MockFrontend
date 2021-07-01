93a11ebf37282606f1d2e50764e7c9e21071c556
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-01 22:27:03 +0530

    database model added

--- dataFlowDashboard/DataSource/admin.py
+++ dataFlowDashboard/DataSource/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book,Transaction,Customer
+
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Transaction)
+admin.site.register(Customer)
--- dataFlowDashboard/DataSource/models.py
+++ dataFlowDashboard/DataSource/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+class Book(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    book_name = models.CharField(max_length=50,null=True)
+    type = models.CharField(max_length=50,null=True)
+    location = models.CharField(max_length=50,null=True)
+
+class Customer(models.Model):
+    cust_id = models.IntegerField(primary_key=True)
+    cust_name = models.CharField(max_length=50,null=True)
+   
+class Transaction(models.Model):
+    transaction_id = models.IntegerField(primary_key=True)
+    book_id = models.ForeignKey(Book,on_delete=models.CASCADE)
+    cust_id = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=50,null=True)
+    transaction_timestamp = models.DateTimeField(default=datetime.datetime.now(),null=True)
+    linked_transaction = models.IntegerField(null=True)
+
+    

9a749efa2f1e1f95f4db8b7481b6d775074c5b80
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-02 10:37:04 +0530

    master branch cleaned

--- dataFlowDashboard/DataSource/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book,Transaction,Customer, ExceptionType
-
-
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Transaction)
-admin.site.register(Customer)
-admin.site.register(ExceptionType)

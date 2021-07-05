57df08bf18440de87c5face8ed8a382e44622ddc
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-05 12:39:18 +0530

    frontend initiated: S

---
+++ frontend/src/Data/filtered.js
@@ -0,0 +1,7 @@
+export const filtered = [
+    { filter_no: 1, filter_layer: "tainbound" },
+    { filter_no: 2, filter_layer: "tainbound" },
+    { filter_no: 3, filter_layer: "tailayer" },
+    { filter_no: 4, filter_layer: "tailayer" },
+
+  ];

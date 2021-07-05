4922e4ea96ca598d7f4d21ea9866b8c9c24fe424
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-05 13:21:09 +0530

    exception data displayed

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+import Exception from './Exception';
+class Dashboard extends Component {
+    state = {  }
+    render() { 
+        return ( 
+          <Exception/>
+         );
+    }
+}
+ 
+export default Dashboard;
---
+++ frontend/src/Components/exception.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import exception from '../Data/exception';
+
+
+class Exception extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {exception};
+    };
+    render() { 
+        return ( 
+            <main className="content">
+            <div className="row">
+              <div className="col-md-6 col-sm-10 mx-auto p-0">
+                <div className="card p-3">
+                  <ul className="list-group list-group-flush">
+                  {this.state.exception.map(item => (
+                  <div>
+                    <h3>{item.exception_name}</h3>
+                    <span>{item.exception_desc}</span>
+                  </div>
+                  ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </main>
+         );
+    }
+}
+ 
+export default Exception;

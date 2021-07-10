b75252fa59f22c5e55bffa1371809502924a6dd9
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-10 09:17:06 +0530

    A Frontend

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -16,20 +16,20 @@ const headStyle={
   height: "24px",
   left: "45px",
   top: "41px",
-  fontFamily: "Quicksand-Medium",
+  fontFamily: "Roboto Mono",
   textDecoration:"none",
   fontWeight: "bold",
-  fontSize: "19px",
+  fontSize: "22px",
   lineHeight: "24px",
   letterSpacing: "0.4px",
-  color: "#A4A6B3",
+  color: "#EEEEEE",
   opacity: "0.7"
 
 
 }
 const navStyle={
   position: "absolute",
-  width: "200px",
+  width: "250px",
   height: "1000px",
   left: "-8px",
   top: "0px",
@@ -37,16 +37,14 @@ const navStyle={
   textDecoration:"none",
 }
 const itemStyle={ 
-  top:"50px",
-  
+  top:"20px",  
   fontFamily: "Quicksand-Normal",
   textDecoration:"none",
   fontWeight: "normal",
-  fontSize: 16,
+  fontSize: "19px",
   lineWeight: "20px",
   letterSpacing: "0.2px",
-  color: "#A4A6B3",
-  
+  color: "#EEEEEE", 
   
 }
 const listStyle={
@@ -65,10 +63,13 @@ class App extends Component {
           <nav style={navStyle}>
           <h3 style={headStyle} >Dashboard</h3>
           <ul  style={listStyle}>
-            <li style={itemStyle}><Link to={'/'}> <img src={aperture} style={{height:14},{width:14},{marginRight:10}}/>Overview </Link></li>
-            <li style={itemStyle}><Link to={'/ExceptionReport'}><img src={alert} style={{height:14},{width:14},{marginRight:10}}/>Exception Report</Link></li>
-            <li style={itemStyle}><Link to={'/FilterReport'}><img src={filter} style={{height:14},{width:14},{marginRight:10}}/>Filter Report</Link></li>
-            <li style={itemStyle}><Link to={''}><img src={check} style={{height:14},{width:14},{marginRight:10}}/>Processed Report</Link></li>
+            <li ><Link to={'/'} style={itemStyle}> <img src={aperture} style={{height:14},{width:14},{marginRight:10}}/>Overview </Link></li>
+            <br></br>
+            <li ><Link to={'/ExceptionReport'} style={itemStyle}><img src={alert} style={{height:14},{width:14},{marginRight:10}}/>Exception Report</Link></li>
+            <br></br>
+            <li><Link to={'/FilterReport'}  style={itemStyle}><img src={filter} style={{height:14},{width:14},{marginRight:10}}/>Filter Report</Link></li>
+            <br></br>
+            <li><Link to={''} style={itemStyle}><img src={check} style={{height:14},{width:14},{marginRight:10}}/>Processed Report</Link></li>
           </ul>
           </nav>
           <hr />

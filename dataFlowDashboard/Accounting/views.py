e178950551c5eaf3f55b1da78864044ad6aaf081
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-15 10:30:28 +0530

    S: Changes in integration

--- dataFlowDashboard/Accounting/views.py
+++ dataFlowDashboard/Accounting/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Accounting.models import AccountingData
 
 # Create your views here.
+def Accounting_Query(): 
+    entry_list = list(AccountingData.objects.filter(Exception_component="Accounting"))
+    return entry_list
+
+    
--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import { Container,Row,Col } from "reactstrap";
-const customStyle={
-position: "relative",
-marginLeft:550,
-height: "50px",
-top: "5px",
-fontFamily: "Playfair Display Black",
-fontStyle: "normal",
-fontWeight: "bold",
-fontSize: "40px",
-borderRadius:0,
-display: "flex",
-alignItems: "center",
-textAlign: "center",
-letterSpacing: "0.5px",
-color: "#000000",  
+/*header styles*/
+function headerStyle() {
+  return {
+    position: "relative",
+    marginLeft: 550,
+    height: "50px",
+    top: "5px",
+    fontFamily: "Playfair Display Black",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "40px",
+    borderRadius: 0,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    letterSpacing: "0.5px",
+    color: "#000000",
+  };
 }
+const customStyle=headerStyle()
 
 function Header() {
   return (
     <header> 
-      <div style={customStyle}><img src={logo} style={{height:60},{width:60}}/><h1 style={{marginLeft:"125px"}}>Dataflow Dashboard</h1></div>     
+      <div style={customStyle}>
+        <img src={logo} style={{height:60},{width:60}}/>
+        <h1 style={{marginLeft:"125px"}}>Dataflow Dashboard</h1>
+      </div>     
     </header>
   );
 }

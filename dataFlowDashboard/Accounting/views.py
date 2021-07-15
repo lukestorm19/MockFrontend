15bbee2498878e31f81767d65933932a3f05069a
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-15 17:11:44 +0530

    Update Exception when accounting is changed

--- dataFlowDashboard/Accounting/views.py
+++ dataFlowDashboard/Accounting/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Accounting.models import AccountingData
-
+from Exceptions.models import ExceptionType
 # Create your views here.
+final_buffer=[]
 def Accounting_Query(): 
     entry_list = list(AccountingData.objects.filter(Exception_component="Accounting"))
-    return entry_list
+    temp_buffer=[]
+    for i in entry_list:
+        if i not in final_buffer:
+            temp_buffer.append(i)
+    for j in temp_buffer:
+        new_entry = ExceptionType(exception_ID=j.Exception_ID,
+        exception_name=j.Exception_name,exception_component=j.Exception_component,
+        exception_level=j.Exceptiom_level,exception_description=j.Exception_description,
+        exception_COBDT=j.Exception_COBDT,exception_LegalEntity=j.Exception_LegalEntity,
+        exception_ProfitCenter=j.Exception_ProfitCenter,exception_BusinessLine=j.Exception_BusinessLine,
+        exception_Region=j.Exception_Region)
+        new_entry.save()
+    final_buffer.extend(temp_buffer)
+
+        
 
     

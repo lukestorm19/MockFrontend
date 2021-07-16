17a97ca6cd2f7d08793681e2673d278f92f5db55
himanshulal2308 <2017uec1671@mnit.ac.in>
2021-07-16 09:21:40 +0530

    L:Celery for Accounting and Exceptions

--- dataFlowDashboard/Accounting/views.py
+++ dataFlowDashboard/Accounting/views.py
@@ -6,19 +6,28 @@ from Exceptions.models import ExceptionType
 final_buffer=[]
 def Accounting_Query(): 
     entry_list = list(AccountingData.objects.filter(Exception_component="Accounting"))
-    temp_buffer=[]
     for i in entry_list:
-        if i not in final_buffer:
-            temp_buffer.append(i)
-    for j in temp_buffer:
-        new_entry = ExceptionType(exception_ID=j.Exception_ID,
-        exception_name=j.Exception_name,exception_component=j.Exception_component,
-        exception_level=j.Exceptiom_level,exception_description=j.Exception_description,
-        exception_COBDT=j.Exception_COBDT,exception_LegalEntity=j.Exception_LegalEntity,
-        exception_ProfitCenter=j.Exception_ProfitCenter,exception_BusinessLine=j.Exception_BusinessLine,
-        exception_Region=j.Exception_Region)
-        new_entry.save()
-    final_buffer.extend(temp_buffer)
+        if not (ExceptionType.objects.filter(exception_ID=i.Exception_ID).exists()):
+            new_entry = ExceptionType(exception_ID=i.Exception_ID,
+            exception_name=i.Exception_name,exception_component=i.Exception_component,
+            exception_level=i.Exceptiom_level,exception_description=i.Exception_description,
+            exception_COBDT=i.Exception_COBDT,exception_LegalEntity=i.Exception_LegalEntity,
+            exception_ProfitCenter=i.Exception_ProfitCenter,exception_BusinessLine=i.Exception_BusinessLine,
+            exception_Region=i.Exception_Region)
+            new_entry.save()
+
+    # for i in entry_list:
+    #     if i not in final_buffer:
+    #         temp_buffer.append(i)
+    # for j in temp_buffer:
+    #     new_entry = ExceptionType(exception_ID=j.Exception_ID,
+    #     exception_name=j.Exception_name,exception_component=j.Exception_component,
+    #     exception_level=j.Exceptiom_level,exception_description=j.Exception_description,
+    #     exception_COBDT=j.Exception_COBDT,exception_LegalEntity=j.Exception_LegalEntity,
+    #     exception_ProfitCenter=j.Exception_ProfitCenter,exception_BusinessLine=j.Exception_BusinessLine,
+    #     exception_Region=j.Exception_Region)
+    #     new_entry.save()
+    # final_buffer.extend(temp_buffer)
 
         
 
